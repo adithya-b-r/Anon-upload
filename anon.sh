953631d8baa1a3670ebc56411ac3f210537cdd0f
Abr <59525997+Adithyabr009@users.noreply.github.com>
2022-04-22 21:12:15 +0530

    Create anon.sh

---
+++ anon.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+read -p "Enter the file name to upload : " f
+
+file=$(pwd)/$f
+
+if [[ -a ${file} ]]; then
+res=$(curl -s -F "file=@$f" https://api.anonfiles.com/upload | jq '.'>
+else
+echo "File $f is not found"
+fi
+
+echo "$res"
+
+if [[ $(uname -o) == "Android" ]]; then
+termux-clipboard-set "$res"
+fi
+
+xdg-open "$res"
