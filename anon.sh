154bf8867215e59aff552d95ab64e87dd8357577
Abr <59525997+Adithyabr009@users.noreply.github.com>
2022-11-10 22:00:17 +0530

    Update anon.sh

--- anon.sh
+++ anon.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-read -p "Enter the file name to upload : " f
+read -p $'\e[33mEnter the file name to upload \e[31m:\e[0m ' f
 
 file=$(pwd)/$f
 
 if [[ -a ${file} ]]; then
-res=$(curl -s -F "file=@$f" https://api.anonfiles.com/upload | jq '.')
+res=$(curl -s -F "file=@$f" https://api.anonfiles.com/upload | jq .data.file.url.short) >/dev/null 2>&1
 else
 echo "File $f is not found"
 fi
 
-echo "$res"
+res=$(echo $res | tr -d '"')
 
 if [[ $(uname -o) == "Android" ]]; then
 termux-clipboard-set "$res"
-fi
-
 xdg-open "$res"
+else
+echo -e "\e[94mLink \e[31m: \e[32m$res\e[0m"
+fi
